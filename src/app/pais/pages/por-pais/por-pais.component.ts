ada917bc2dcaa9b43afc51b88249e74931e29626
Fer1196 <rodifer1196@hotmail.com>
2021-11-02 02:05:42 -0500

    Lista de sugerencias del proyecto

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,13 +6,20 @@ import { Country } from '../../interfaces/pais.interface';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+    li{
+      cursor:pointer;
+    }
+    `
   ]
 })
 export class PorPaisComponent  {
-
+  
   termino:string = ''
   hayError:boolean = false;
   paises:Country[]=[];
+  paisesSugeridos:Country[]=[];
+  mostrarSugerencias:boolean =false;
 
   constructor(private paisService:PaisService){}
 
@@ -37,7 +44,20 @@ export class PorPaisComponent  {
 
   sugerencias(termino:string){
       this.hayError=false; 
+      this.termino=termino
+      this.mostrarSugerencias=true;
+
+      this.paisService.buscarPais(termino)
+      .subscribe(paises=>this.paisesSugeridos=paises.splice(0,4),
+      (err)=>this.paisesSugeridos=[]
       
+      )
+      
+  }
+
+  buscarSugeridos(termino:string){
+    this.buscar(termino); 
+    this.mostrarSugerencias=false;
   }
 
 }
